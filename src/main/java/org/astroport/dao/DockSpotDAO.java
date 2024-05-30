b03bc30ae501c954b3b18b65dbf813872f474c53
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-30 21:17:25 +0200

    Ajout des fichiers d'intéractions avec la BDD

---
+++ src/main/java/org/astroport/dao/DockSpotDAO.java
@@ -0,0 +1,57 @@
+package org.astroport.dao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.astroport.config.DatabaseConfig;
+import org.astroport.constants.DatabaseQueries;
+import org.astroport.constants.DockType;
+import org.astroport.model.DockSpot;
+
+import java.sql.*;
+import java.util.Optional;
+
+
+public class DockSpotDAO {
+    private static final Logger logger = LogManager.getLogger("DockSpotDAO");
+
+    public DatabaseConfig databaseConfig = new DatabaseConfig();
+
+
+    public Optional<Integer> getNextAvailableSlot(DockType dockType) throws Exception {
+        Optional<Integer> result = Optional.empty();
+        try (
+                Connection con = databaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DatabaseQueries.QUERY_GET_NEXT_AVAILABLE_DOCK)
+        ) {
+            ps.setString(1, dockType.toString());
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    result = Optional.of(rs.getInt(1));
+                }
+            }
+        } catch (SQLException ex) {
+            logger.error("Error fetching dock number from DB. Parking slots might be full", ex);
+        }
+        return result;
+    }
+
+
+    public boolean updateDock(DockSpot dockSpot) {
+        try (
+                Connection connection = databaseConfig.getConnection();
+                PreparedStatement statement = connection.prepareStatement(DatabaseQueries.QUERY_UPDATE_DOCK_AVAILABILITY)
+        ) {
+
+            statement.setBoolean(1, dockSpot.isAvailable());
+            statement.setInt(2, dockSpot.getId());
+
+            int updatedRows = statement.executeUpdate();
+            return updatedRows == 1;
+
+        } catch (SQLException ex) {
+            logger.error("Error updating dock info", ex);
+            return false;
+        }
+    }
+}
+
---
+++ src/main/java/org/astroport/dao/TicketDAO.java
@@ -0,0 +1,102 @@
+package org.astroport.dao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.astroport.config.DatabaseConfig;
+import org.astroport.constants.DatabaseQueries;
+import org.astroport.constants.DockType;
+import org.astroport.model.DockSpot;
+import org.astroport.model.Ticket;
+
+import java.sql.*;
+
+public class TicketDAO {
+
+    private static final Logger logger = LogManager.getLogger("TicketDAO");
+
+    public DatabaseConfig databaseConfig = new DatabaseConfig();
+
+
+    public boolean saveTicket(Ticket ticket) {
+        try (
+                Connection con = databaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DatabaseQueries.QUERY_INSERT_NEW_TICKET)
+        ) {
+            ps.setInt(1, ticket.getParkingSpot().getId());
+            ps.setString(2, ticket.getVehicleRegNumber());
+            ps.setDouble(3, ticket.getPrice());
+            ps.setTimestamp(4, Timestamp.valueOf(ticket.getInTime()));
+            ps.setTimestamp(5, (ticket.getOutTime() == null) ? null : Timestamp.valueOf(ticket.getOutTime()));
+            return ps.execute();
+        } catch (SQLException ex) {
+            logger.error("Error saving ticket info", ex);
+            return false;
+        }
+    }
+
+
+    public int getNbTicket(String vehicleRegNumber) {
+        int nbTickets = 0;
+        try (
+                Connection con = databaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DatabaseQueries.QUERY_GET_TICKET_COUNT_BY_SHIP_REG_NUMBER);
+        ) {
+            ps.setString(1, vehicleRegNumber);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) nbTickets = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            logger.error("Error fetching ticket's number", ex);
+        }
+        return nbTickets;
+    }
+
+
+    public Ticket getTicket(String vehicleRegNumber) {
+        Ticket ticket = null;
+        try (
+                Connection con = databaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DatabaseQueries.QUERY_GET_TICKET_BY_SHIP_REG_NUMBER)
+        ) {
+            ps.setString(1, vehicleRegNumber);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    ticket = new Ticket();
+                    DockSpot parkingSpot = new DockSpot(rs.getInt(1), DockType.valueOf(rs.getString(6)), false);
+                    ticket.setParkingSpot(parkingSpot);
+                    ticket.setId(rs.getInt(2));
+                    ticket.setVehicleRegNumber(vehicleRegNumber);
+                    ticket.setPrice(rs.getDouble(3));
+
+                    Timestamp inTime = rs.getTimestamp(4);
+                    ticket.setInTime((inTime != null) ? inTime.toLocalDateTime() : null);
+                    Timestamp outTime = rs.getTimestamp(5);
+                    ticket.setOutTime((outTime != null) ? outTime.toLocalDateTime() : null);
+                }
+            }
+        } catch (SQLException ex) {
+            logger.error("Error fetching next available slot", ex);
+        }
+        return ticket;
+    }
+
+
+    public boolean updateTicket(Ticket ticket) {
+        try (
+                Connection con = databaseConfig.getConnection();
+                PreparedStatement ps = con.prepareStatement(DatabaseQueries.QUERY_UPDATE_TICKET_PRICE_AND_OUT_TIME)
+        ) {
+
+            ps.setDouble(1, ticket.getPrice());
+            ps.setTimestamp(2, Timestamp.valueOf(ticket.getOutTime()));
+            ps.setInt(3, ticket.getId());
+            ps.execute();
+            return true;
+
+        } catch (SQLException ex) {
+            logger.error("Error saving ticket info", ex);
+        }
+        return false;
+    }
+}
