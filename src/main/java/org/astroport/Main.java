ccb2bc70258cbf63d45515f692ad476b29c98893
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-30 21:18:01 +0200

    Ajout du lancement de l'application

--- src/main/java/org/astroport/Main.java
+++ src/main/java/org/astroport/Main.java
@@ -1,7 +1,9 @@
 package org.astroport;
 
+import org.astroport.service.InteractiveShell;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InteractiveShell.loadInterface();
     }
 }
