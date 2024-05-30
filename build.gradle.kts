52731252f9cf15fc86a5775632a1c444f4d4a07f
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-30 21:17:03 +0200

    Modifications de la config Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,47 @@
-plugins {
-    java
-}
 
 group = "org.astroport"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    java
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+application {
+    mainClass.set("org.astroport.Main")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.mockito:mockito-core:5.4.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.4.0")
+    testImplementation("org.assertj:assertj-core:3.26.0")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+}
+
+
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
