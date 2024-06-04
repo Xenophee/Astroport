42bbddd14a3fc6960b23ab8579eb4bb30f5142f7
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-06-04 22:23:11 +0200

    Fusion de constantes DockType et Fare

--- src/main/java/org/astroport/constants/DockType.java
+++ src/main/java/org/astroport/constants/DockType.java
@@ -1,9 +1,19 @@
 package org.astroport.constants;
 
 public enum DockType {
-    CORVETTE,
-    DESTROYER,
-    CRUISER,
-    TITAN,
-    COLOSSUS
+    CORVETTE(1.0),
+    DESTROYER(1.5),
+    CRUISER(2.0),
+    TITAN(2.5),
+    COLOSSUS(3.0);
+
+    private final double ratePerHour;
+
+    DockType(double ratePerHour) {
+        this.ratePerHour = ratePerHour;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
 }
--- src/main/java/org/astroport/constants/Fare.java
+++
@@ -1,9 +0,0 @@
-package org.astroport.constants;
-
-public class Fare {
-    public static final double CORVETTE_RATE_PER_HOUR = 1.0;
-    public static final double DESTROYER_RATE_PER_HOUR = 1.5;
-    public static final double CRUISER_RATE_PER_HOUR = 2.0;
-    public static final double TITAN_RATE_PER_HOUR = 2.5;
-    public static final double COLOSSUS_RATE_PER_HOUR = 3.0;
-}
